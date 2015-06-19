e03ab5d0cc01c6f440bce7b09e1e3f239e3fef11
Matt Williams <matt.williams@exacttarget.com>
2015-06-19 17:38:28 -0400

    Updated to use master branch

--- app/views/tracker.js
+++ app/views/tracker.js
@@ -3,14 +3,8 @@
 angular.module( 'QualityABTracker.tracker', [ 'ngRoute', 'QualityABTracker.controllers' ] )
 
 .config([ '$routeProvider', function( $routeProvider ) {
-	$routeProvider
-
-	.when( '/tracker', {
-		templateUrl: '/app/templates/tracker.html'
-		, controller: 'trackerCtrl'
-	})
-	.when( '/tracker/:player', {
-		templateUrl: '/app/templates/tracker.html'
+	$routeProvider.when( '/tracker', {
+		templateUrl: '/app/templates/tracker.html?' + new Date().getTime()
 		, controller: 'trackerCtrl'
 	})
 }]);
